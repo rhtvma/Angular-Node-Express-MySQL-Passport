7d6be6b971dc5b5951a5327dfb5fa1615ce2b0b4
rhtvma\rohit <rhtvma@outlook.com>
2018-09-14 16:26:28 +0530

    http://localhost:4200/#/home/portfolio
    http://localhost:4200/#/home/portfolio
    
    nested child (Routing)

--- public/src/app/pages/home/home.module.ts
+++ public/src/app/pages/home/home.module.ts
@@ -7,13 +7,11 @@ const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
-        children: [
-            {
-                path: 'portfolio',
-                loadChildren: './portfolio/portfolio.module#PortfolioModule'
-            }
-        ]
     },
+    {
+        path: ':portfolio',
+        loadChildren: './portfolio/portfolio.module#PortfolioModule'
+    }
 ]
 
 @NgModule({
