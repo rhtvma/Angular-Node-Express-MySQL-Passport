1f2713db2bb90103daabd29aa944b8b7d5eb1951
rhtvma\rohit <rhtvma@outlook.com>
2018-09-16 23:47:25 +0530

    minor modifications

--- public/src/app/pages/pages.component.ts
+++ public/src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-pages',
@@ -6,11 +7,12 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./pages.component.css']
 })
 export class PagesComponent implements OnInit {
-
-    constructor() {
+    constructor(public router: Router) {
     }
 
     ngOnInit() {
+        if (this.router.url === '/') {
+            this.router.navigate(['/home']);
+        }
     }
-
 }
